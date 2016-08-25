2e1c9f63fb833fcf2a4f2eda60ba7cf2fe29efe4
Belén Albeza <belen.albeza@gmail.com>
2016-08-25 20:12:13 +0200

    Update 2016-08-25T18:12:10.904Z

--- js/bundle.js
+++ js/bundle.js
@@ -1,3 +1,4 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
 var PlayScene = require('./play_scene.js');
@@ -48,3 +49,18 @@ window.onload = function () {
 
     game.state.start('boot');
 };
+
+},{"./play_scene.js":2}],2:[function(require,module,exports){
+'use strict';
+
+var PlayScene = {
+    create: function () {
+        var logo = this.game.add.sprite(
+            this.game.world.centerX, this.game.world.centerY, 'logo');
+            logo.anchor.setTo(0.5, 0.5);
+    }
+};
+
+module.exports = PlayScene;
+
+},{}]},{},[1]);
